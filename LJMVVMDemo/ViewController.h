0a1392fad32f1c92d52cd9cee05a41039a59a182
clj <caolongjian@vlbuilding.com>
2016-03-17 17:30:26 +0800

    init

---
+++ LJMVVMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJMVVMDemo
+//
+//  Created by Caolongjian on 16/3/17.
+//  Copyright © 2016年 Caolongjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
