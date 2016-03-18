d77e9639a2b7026f41ee9de3b99f38998d278d0e
Caolongs <Caolongs@126.com>
2016-03-18 23:20:02 +0800

    tableView

---
+++ LJMVVMDemo/Tools/LJString.h
@@ -0,0 +1,13 @@
+//
+//  LJString.h
+//  LJMVVMDemo
+//
+//  Created by Caolongjian on 16/3/18.
+//  Copyright © 2016年 Caolongjian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LJString : NSObject
+
+@end
